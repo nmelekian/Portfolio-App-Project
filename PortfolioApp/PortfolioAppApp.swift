45f43ae22d90e316f5956f97cca0c03c6f860f1a
Nick Melekian <nick.melekian@gmail.com>
2025-03-11 13:14:11 -0400

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Nick Melekian on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
